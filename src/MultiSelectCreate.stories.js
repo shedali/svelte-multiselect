fdb3a1e694957cc6fb8c651626a7b121aa3bd22f
Franz <shedali@gmail.com>
2019-12-09 21:55:42 +0000

    add storybook docs

--- src/MultiSelectCreate.stories.js
+++ src/MultiSelectCreate.stories.js
@@ -1,22 +1,46 @@
+import { array, boolean, button, date, files, number, optionsKnob, radios, select, text, withKnobs } from "@storybook/addon-knobs";
+
 import MultiSelectCreate from "./components/MultiSelectCreate.svelte";
+
 export default {
-	title: "MultiSelectCreate"
+	title: "MultiSelectCreate",
+	decorators: [withKnobs]
 };
 
-export const create = () => ({
+export const createEnabled = () => ({
 	Component: MultiSelectCreate,
 	props: {
 		canCreate: true
 	}
 });
 
+createEnabled.story = {
+	name: 'MultiSelect with new item creation enabled',
+	parameters: {
+		notes: `
+	    <h2>Create if item does not exist</h2>
+		If the entered item does not match any existing items, user is prompted to add a new item
+	  `,
+	},
+};
+
+
 export const SelectedItem = () => ({
 	Component: MultiSelectCreate,
 
 	props: {
-		selection: ["Hello"]
+		selection: [text('selected item')]
 	}
 });
+SelectedItem.story = {
+	name: 'Multi Select with an item pre-selected',
+	parameters: {
+		notes: `
+		This one has a preselected item. Use the knobs panel to change the value
+	  `,
+	},
+};
+
 
 export const noCreate = () => ({
 	Component: MultiSelectCreate,
